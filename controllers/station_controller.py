86d56f46b03a5d591290753c103e0e294be13fda
OmegaCoder1 <robson.uefs.2013@gmail.com>
2025-04-02 15:14:39 -0300

    retirada de arquivos de backup e atualização de controller station

--- controllers/station_controller.py
+++ controllers/station_controller.py
@@ -5,208 +5,123 @@ class StationController:
     def _init_(self):
         """Inicializa as estações de recarga disponíveis."""
         self.charging_stations = {
-            "Posto 1": {"x": 2.0, "y": 3.0, "ocupado": False, "tempo_expiracao": None,"id": None, "queue" : []},
-            "Posto 2": {"x": 5.0, "y": 7.0, "ocupado": False, "tempo_expiracao": None,"id": None, "queue" : []},
-            "Posto 3": {"x": 8.0, "y": 2.0, "ocupado": False, "tempo_expiracao": None,"id": None, "queue" : []},
-            "Posto 4": {"x": 1.0, "y": 9.0, "ocupado": False, "tempo_expiracao": None,"id": None, "queue" : []},
-            "Posto 5": {"x": 6.0, "y": 5.0, "ocupado": False, "tempo_expiracao": None,"id": None, "queue" : []},
+            "Posto 1": {"x": 2.0, "y": 3.0, "ocupado": False, "tempo_expiracao": None,"id": None, "reservation_data": None},
+            "Posto 2": {"x": 5.0, "y": 7.0, "ocupado": False, "tempo_expiracao": None,"id": None, "reservation_data": None},
+            "Posto 3": {"x": 8.0, "y": 2.0, "ocupado": False, "tempo_expiracao": None,"id": None, "reservation_data": None},
+            "Posto 4": {"x": 1.0, "y": 9.0, "ocupado": False, "tempo_expiracao": None,"id": None, "reservation_data": None},
+            "Posto 5": {"x": 6.0, "y": 5.0, "ocupado": False, "tempo_expiracao": None,"id": None, "reservation_data": None},
         }
+        
 
 
-
-    #FUNCAO PARA RETORNAR TODAS AS STATIONS (MESMO OCUPADAS)
-    def get_all_stations(self):
-        """Retorna todos os postos."""
-        return self.charging_stations.copy()
-
-
-    def liberar_estacoes_expiradas(self):
-        """Libera todas as estações cujos tempos de expiração já passaram."""
-        try:
-            for nome, dados in self.charging_stations.items():
-                # Verifica se a estação está ocupada e se o tempo de expiração já passou
-                if dados["ocupado"] and dados["tempo_expiracao"] and time.time() > dados["tempo_expiracao"]:
-                    # Libera a estação
-                    self.reset_station(nome)
-                    print(f"A estação {nome} foi liberada devido ao tempo expirado.")
-            return {"status": "sucesso", "mensagem": "Estações expiradas foram liberadas."}
-        except Exception as e:
-            return {"status": "erro", "mensagem": f"Erro ao liberar estações expiradas: {str(e)}"}
-
-
-
-    # FUNCOES PARA RETORNAR OS POSTOS DISPONIVEIS E TAMBEM O POSTO MAIS PROXIMO (JA RESERVA)
+    #FUNCOES PARA RETORNAR OS POSTOS DISPONIVEIS E TAMBEM O POSTO MAIS PROXIMA (JA RESERVA)
     def get_available_stations(self):
         """Retorna os postos disponíveis (não ocupados)."""
-        try:
-            return {
-                nome: dados for nome, dados in self.charging_stations.items()
-                if not dados["ocupado"] or (dados["tempo_expiracao"] and time.time() > dados["tempo_expiracao"])
-            }
-        except Exception as e:
-            return {
-                "status": "erro",
-                "mensagem": f"Erro ao obter postos disponíveis: {str(e)}"
-            }
-
-    def get_station_mais_proximo(self, x, y, id):
+        return {
+            nome: dados for nome, dados in self.charging_stations.items()
+            if not dados["ocupado"] or (dados["tempo_expiracao"] and time.time() > dados["tempo_expiracao"])
+        }
+        
+        
+    def get_station_mais_proximo(self, x, y,id):
         """Retorna o nome do posto mais próximo (não ocupado) e marca como ocupado por 2 minutos."""
-        try:
-            postos_disponiveis = self.get_available_stations()  # Obtém os postos disponíveis apenas uma vez
+        
+        postos_disponiveis = self.get_available_stations()  #  Obtém os postos disponíveis apenas uma vez
+        
+        if not postos_disponiveis:  #  Se não houver postos disponíveis, retorna None
+            return None  
 
-            if not postos_disponiveis:  # Se não houver postos disponíveis, retorna None
-                menorTempo = 9999999
-                for posto in self.charging_stations:
-                    tempo = len(posto["queue"]) * 80 + calcular_distancia(x, y, self.charging_stations[posto]["x"], self.charging_stations[posto]["y"])
-                    if tempo < menorTempo:
-                        menorTempo = tempo
-                        station_mais_proximo = posto
-                
-                self.set_station_occupied(station_mais_proximo,id)  #  Marca o posto como ocupado
-                return {
-                    "status": "Sucess",
-                    "mensagem": "Posto reservado (fila) {posto}"
-                }
-
-            station_mais_proximo = min(
-                postos_disponiveis,  # Agora usa a lista já obtida
-                key=lambda nome: calcular_distancia(x, y, self.charging_stations[nome]["x"], self.charging_stations[nome]["y"])
-            )
-
-            self.set_station_occupied(station_mais_proximo, id)  # Marca o posto como ocupado
-
-            return {
-                "status": "sucesso",
-                "mensagem": f"O posto mais próximo disponível é: {station_mais_proximo}"
-            }
-        except Exception as e:
-            return {
-                "status": "erro",
-                "mensagem": f"Erro inesperado: {str(e)}"
-            }
-
+        station_mais_proximo = min(
+            postos_disponiveis,  #  Agora usa a lista já obtida
+            key=lambda nome: calcular_distancia(x, y, self.charging_stations[nome]["x"], self.charging_stations[nome]["y"])
+        )
+        
+        self.set_station_occupied(station_mais_proximo,id)  #  Marca o posto como ocupado
+        
+        return {
+            "status": "sucesso",
+            "mensagem": f"O posto mais próximo disponível é: {station_mais_proximo}"
+        }
+        
     ###########################################################################################
 
-    # DEFINE O STATION COMO OCUPADO ############################################################
-    def set_station_occupied(self, station_name, id):
+
+
+
+    
+    
+    #DEFINE O STATION COMO OCUPADO ############################################################
+    def set_station_occupied(self, station_name,id):
         """Marca um posto como ocupado por 2 minutos."""
-        try:
-            if station_name in self.charging_stations:
-                print(f"mudou o status de : {station_name} para true")
-                self.charging_stations[station_name]["ocupado"] = True
-                self.charging_stations[station_name]["tempo_expiracao"] = time.time() + 120  # 2 minutos
-                self.charging_stations[station_name]["reservation_data"] = time.time()
-                self.charging_stations[station_name]["id"] = id
-                self.charging_stations[station_name]["queue"].append(id)
-                print(f"print de todos os postos atuais: {self.charging_stations}")
-        except Exception as e:
-            print(f"Erro ao marcar posto como ocupado: {str(e)}")
+        if station_name in self.charging_stations:
+            print(f"mudou o status de : {station_name} para true")
+            self.charging_stations[station_name]["ocupado"] = True
+            self.charging_stations[station_name]["tempo_expiracao"] = time.time() + 120  # 2 minutos
+            self.charging_stations[station_name]["reservation_data"] = time.time()
+            self.charging_stations[station_name]["id"] = id
+            print(f"print de todos os postos atuais: {self.charging_stations}")
             
     ###########################################################################################
+
+
+
+
 
     # DESOCUPAR UMA STATION PELO NOME DELA (USAR COMO ADM PARA LIBERAR MANUALMENTE A ESTAÇÃO)
     def reset_station(self, station_name):
         """Libera um posto manualmente."""
-        try:
-            if station_name in self.charging_stations:
-                self.charging_stations[station_name]["ocupado"] = False
-                self.charging_stations[station_name]["tempo_expiracao"] = None
-                self.charging_stations[station_name]["reservation_data"] = None
-                self.charging_stations[station_name]["id"] = None
-        except Exception as e:
-            print(f"Erro ao liberar a estação: {str(e)}")
+        if station_name in self.charging_stations:
+            self.charging_stations[station_name]["ocupado"] = False
+            self.charging_stations[station_name]["tempo_expiracao"] = None
+            self.charging_stations[station_name]["reservation_data"] = None
+            self.charging_stations[station_name]["id"] = None
             
     ###########################################################################################
-
+    
+    
+    
+    
+    
     # RETORNA TODAS AS ESTAÇÕES OCUPADAS POR UM ID ############################
     def get_stations_by_id(self, id_usuario):
-        """Retorna todas as estações ocupadas por um determinado ID e a mensagem com a estação mais próxima."""
-
-        try:
-            # Filtra as estações ocupadas com o id_usuario correspondente
-            stations_ocupadas = {
-                nome: dados for nome, dados in self.charging_stations.items()
-                if dados.get("ocupado") and dados.get("id") == id_usuario
-            }
-
-            if stations_ocupadas:
-                # Considerando a primeira estação ocupada como a mais próxima
-                station_mais_proximo = list(stations_ocupadas.keys())[0]
-                return {
-                    "status": "sucesso",
-                    "mensagem": f"Postos reservados: {station_mais_proximo}, {stations_ocupadas}"
-                }
-            else:
-                # Caso não haja estações ocupadas para o usuário
-                return {
-                    "status": "erro",
-                    "mensagem": "Nenhuma estação ocupada encontrada para o usuário."
-                }
-        except Exception as e:
-            return {
-                "status": "erro",
-                "mensagem": f"Erro inesperado: {str(e)}"
-            }
+        """Retorna todas as estações ocupadas por um determinado ID."""
+        return {
+            nome: dados for nome, dados in self.charging_stations.items()
+            if dados.get("ocupado") and dados.get("id") == id_usuario
+        }
     ###########################################################################################
+    
+    
 
     # LIBERA TODAS AS ESTAÇÕES OCUPADAS POR UM ID ##############################################
     def release_stations_by_id(self, id_usuario):
-        """Libera todas as estações ocupadas por um determinado ID e retorna status no formato JSON."""
-        try:
-            stations_lib = []  # Lista para armazenar os nomes das estações liberadas
-
-            for nome, dados in self.charging_stations.items():
-                if dados.get("ocupado") and dados.get("id") == id_usuario:
-                    self.reset_station(nome)  # Usa a função existente para liberar o posto
-                    stations_lib.append(nome)
-
-            if stations_lib:
-                return {
-                    "status": "sucesso",
-                    "mensagem": f"As estações liberadas para o usuário {id_usuario} são: {', '.join(stations_lib)}"
-                }
-            else:
-                # Caso não haja estações ocupadas para o usuário
-                return {
-                    "status": "erro",
-                    "mensagem": "Nenhuma estação ocupada encontrada para o usuário."
-                }
-        except Exception as e:
-            return {
-                "status": "erro",
-                "mensagem": f"Erro inesperado ao liberar as estações: {str(e)}"
-            }
+        """Libera todas as estações ocupadas por um determinado ID."""
+        for nome, dados in self.charging_stations.items():
+            if dados.get("ocupado") and dados.get("id") == id_usuario:
+                self.reset_station(nome)  # Usa a função existente para liberar o posto
     ###########################################################################################
-
-    # FUNCAO PARA LIBERAR TODOS OS POSTOS, SEM PRECISAR DE ID (USADO COMO ADM)
+    
+    #FUNCAO PARA LIBERAR TODOS OS POSTOS, SEM PRECISAR DE ID (USADO COMO ADM)
     def release_all_stations(self):
-        """Libera todos os postos ocupados, sem precisar de ID de usuário e retorna status no formato JSON."""
-        try:
-            stations_lib = []  # Lista para armazenar os nomes das estações liberadas
+        """Libera todos os postos ocupados, sem precisar de ID de usuário."""
+        for nome, dados in self.charging_stations.items():
+            if dados.get("ocupado"):
+                self.reset_station(nome)  # Usa a função existente para liberar o posto
+                return "TODOS LIBERADOS"
 
-            for nome, dados in self.charging_stations.items():
-                if dados.get("ocupado"):
-                    self.reset_station(nome)  # Usa a função existente para liberar o posto
-                    stations_lib.append(nome)
 
-            if stations_lib:
-                return {
-                    "status": "sucesso",
-                    "mensagem": f"As estações liberadas são: {', '.join(stations_lib)}"
-                }
-            else:
-                # Caso não haja estações ocupadas
-                return {
-                    "status": "erro",
-                    "mensagem": "Nenhuma estação ocupada foi encontrada para liberar."
-                }
-        except Exception as e:
-            return {
-                "status": "erro",
-                "mensagem": f"Erro inesperado ao liberar todas as estações: {str(e)}"
-            }
 
-# funções gerais:
+
+
+
+
+
+
+
+
+
+
+#funcoes gerais:
 def calcular_distancia(x1, y1, x2, y2):
     """Calcula a distância Euclidiana entre dois pontos."""
-    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    return math.sqrt((x2 - x1) * 2 + (y2 - y1) * 2)
--- teste_posto_ocupado.py
+++
@@ -1,4 +0,0 @@
-from service.socket_service import send_to_container, get_recharge_station
-# Exemplo de uso
-print(get_recharge_station(4, 5))  # Deve retornar o posto mais próximo disponível
-print(get_recharge_station(7, 6))  # Deve retornar outro posto disponível, já que o anterior está ocupado
